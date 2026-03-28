8f32c92ed2cc7142fa5983a50e50acb73569b203
Ikhmal Hanif <ikhmalhanif60@gmail.com>
2026-03-29 01:12:32 +0800

    Fix Vercel analytics build type error

---
+++ components/vercel-analytics.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { inject } from "@vercel/analytics";
+import { useEffect } from "react";
+
+export function VercelAnalytics() {
+  useEffect(() => {
+    inject();
+  }, []);
+
+  return null;
+}
